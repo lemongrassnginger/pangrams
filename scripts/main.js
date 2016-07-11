eade401d0af9af89ad958712fa9ffb041c784bbf
stuart.bell@bluefinsolutions.com <stuart.bell@bluefinsolutions.com>
2016-07-11 23:58:35 +0100

    Add button and handler

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,14 @@ myHeading.textContent = 'Hello world!';
 document.querySelector('html').onclick = function() {
     alert('Ouch! Stop poking me!');
 }
+
+function setUserName () {
+	var sName = prompt('Please enter your name.');
+	localStorage.setItem('name', sName); //persist name using localStorage API
+	myHeading.textContent = "Hello " + sName + "!";
+}
+
+var oButton = document.querySelector('button');
+oButton.onclick = function() {
+	setUserName();
+}
